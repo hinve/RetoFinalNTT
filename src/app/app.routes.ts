e0085fa590f0e17e9a2ab7d8c2ca3bb821329d3f
SergioLM-educarex <sleonm03@educarex.es>
2025-06-20 12:09:32 +0200

    arreglo
    sergio commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './landing-page/landing-page';
+import { ClientesComponent } from './components/clientes-component/clientes-component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: LandingPage }, // Ruta inicial (landing)
+    { path: '/clientes' , component:ClientesComponent, pathMatch: 'full' },
+ 
+];
