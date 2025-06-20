c0f95d22797b9f056e135e2ed2c0eb75f2cc53c2
SergioLM-educarex <sleonm03@educarex.es>
2025-06-20 11:30:54 +0200

    landingPage
    commit sergio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ClientesComponent } from './components/clientes-component/clientes-component';
+import { LandingPage } from './landing-page/landing-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ClientesComponent],
+  imports: [RouterOutlet, ClientesComponent, LandingPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
