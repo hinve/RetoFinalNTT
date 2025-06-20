05ce306d5c3ef8b82daacb04e7b32054ec59af01
AlvaroCarrion <alvarocarrion.git@gmail.com>
2025-06-20 11:53:52 +0200

    añadido a header la lista de clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ClientesComponent } from './components/clientes-component/clientes-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/clientes' , component:ClientesComponent, pathMatch: 'full' },
+];
